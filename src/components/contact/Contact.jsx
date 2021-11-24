b89e6529929688ec63c61c4508674af1c8ff06a1
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-11-25 03:05:37 +0600

    add contact me page and EmailJS

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,75 @@
+import './Contact.css'
+import Phone from '../../img/phone.png'
+import Email from '../../img/email.png'
+import Address from '../../img/address.png'
+import { useRef, useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const Contact = () => {
+    const formRef = useRef()
+    const [done, setDone] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'service_stwl82b',
+        'template_cf0kgk3',
+        formRef.current,
+        'user_52j43WzOUkR5StKKPvkAf'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+  return (
+    <div className='c'>
+      <div className='c-bg'></div>
+      <div className='c-wrapper'>
+        <div className='c-left'>
+          <h1 className='c-title'>Let's discuss your project</h1>
+          <div className='c-info'>
+            <div className='c-info-item'>
+              <img src={Phone} alt='' className='c-icon' />
+              +996 702 66 88 22
+            </div>
+            <div className='c-info-item'>
+              <img src={Email} alt='' className='c-icon' />
+              ruslanorozobaev@gmail.com
+            </div>
+            <div className='c-info-item'>
+              <img src={Address} alt='' className='c-icon' />
+              Bishkek, Kyrgyzstan
+            </div>
+          </div>
+        </div>
+        <div className='c-right'>
+          <p className='c-desc'>
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type='text' placeholder='Name' name='user_name' />
+            <input type='text' placeholder='Subject' name='user_subject' />
+            <input type='text' placeholder='Email' name='user_email' />
+            <textarea
+              rows='5'
+              placeholder='Send me your questions and feedback'
+              name='message'
+            />
+            <button>Submit</button>
+            {done && "Thank you... =)"}
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
