1c0a7daf223786e93ca98192036755cff2d63d48
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-11-25 01:12:37 +0600

    finish product list page

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/1nfQBQy/project.png",
+      link: "https://cvrussab.netlify.app/",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/1nfQBQy/project.png",
+      link: "https://cvrussab.netlify.app/",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/1nfQBQy/project.png",
+      link: "https://cvrussab.netlify.app/",
+    },
+    {
+      id: 4,
+      img: "https://i.ibb.co/1nfQBQy/project.png",
+      link: "https://cvrussab.netlify.app/",
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/1nfQBQy/project.png",
+      link: "https://cvrussab.netlify.app/",
+    },
+    {
+      id: 6,
+      img: "https://i.ibb.co/1nfQBQy/project.png",
+      link: "https://cvrussab.netlify.app/",
+    },
+  ];
