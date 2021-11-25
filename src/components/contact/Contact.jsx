577280df11bb14612daa002c5e6e1239e60a7ab8
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-11-25 20:38:15 +0600

    add DarkMode (ContextApi)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,15 @@ import './Contact.css'
 import Phone from '../../img/phone.png'
 import Email from '../../img/email.png'
 import Address from '../../img/address.png'
-import { useRef, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import emailjs from 'emailjs-com'
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
     const formRef = useRef()
     const [done, setDone] = useState(false)
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -55,10 +58,11 @@ const Contact = () => {
             freelancing if the right project comes along. me.
           </p>
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input type='text' placeholder='Name' name='user_name' />
-            <input type='text' placeholder='Subject' name='user_subject' />
-            <input type='text' placeholder='Email' name='user_email' />
+            <input style={{backgroundColor: darkMode && "#333"}} type='text' placeholder='Name' name='user_name' />
+            <input style={{backgroundColor: darkMode && "#333"}}type='text' placeholder='Subject' name='user_subject' />
+            <input style={{backgroundColor: darkMode && "#333"}}type='text' placeholder='Email' name='user_email' />
             <textarea
+              style={{backgroundColor: darkMode && "#333"}}
               rows='5'
               placeholder='Send me your questions and feedback'
               name='message'
