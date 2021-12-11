677eb7403a4ec3763b865bc206ae41fee20ad94c
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2021-12-11 19:43:31 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
-import Intro from "./components/intro/Intro";
+import Intro from "./components/Intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Toggle from "./components/toggle/Toggle";
 import { ThemeContext } from "./context";
